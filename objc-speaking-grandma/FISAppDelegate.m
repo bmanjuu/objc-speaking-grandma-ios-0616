389fc0b58fe47bdca79dab05a7e61238417ec664
bmanjuu <bettyofung@gmail.com>
2016-05-31 15:31:21 -0400

    second pass at speaking grandma lab

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -7,11 +7,46 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    // PART I
+    NSString *talkToGrandma = @"HI, GRANDMA!";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma]; //boolean holds the evaluation of the isEqualToString method from the point in our code at which it was last set
+    //result of boolean 
+    
+    talkToGrandma = @"Hi, grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+    if(shouting){
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    else {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    
+    // PART II
+    if([talkToGrandma isEqualToString:shoutAtGrandma]){
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    else {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    
+    // PART III
+    if (!shouting) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
     
     return YES; // Don't alter this statement.
 }
